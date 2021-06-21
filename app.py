38bc1d55b32bfc99d85d21a888d8c7f3db0aeae8
HANNANI Mohamed <49957450+mhannani@users.noreply.github.com>
2021-06-21 22:16:37 +0100

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import dash
 import dash_html_components as html
 import music_component
+import os
+import flask
+
 external_stylesheets = ["https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css"]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+css_directory = os.getcwd()
+stylesheets = ['app.css']
+static_css_route = '/assets/'
 
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
     music_component.MusicComponent(),
@@ -16,6 +22,21 @@ app.layout = html.Div([
         children="Fork me on GitHub"
     )
 ])
+
+
+@app.server.route('{}<stylesheet>'.format(static_css_route))
+def serve_stylesheet(style):
+    if style not in stylesheets:
+        raise Exception(
+            '"{}" is excluded from the allowed static files'.format(
+                style
+            )
+        )
+    return flask.send_from_directory(css_directory, style)
+
+
+for stylesheet in stylesheets:
+    app.css.append_css({"external_url": "/assets/{}".format(stylesheet)})
 
 if __name__ == '__main__':
     # for deployment
